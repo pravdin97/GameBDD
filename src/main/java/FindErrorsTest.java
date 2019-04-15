615d62efeb64245630bf7a411e2be45fee448866
pravdin97 <pravdin.ev97@gmail.com>
2019-04-15 16:16:29 +0700

    Написаны тесты к сценарию. Тесты не проходят

--- src/main/java/FindErrorsTest.java
+++ src/main/java/FindErrorsTest.java
@@ -1,22 +1,42 @@
+import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.junit.Assert;
+
+import java.util.ArrayList;
 
 public class FindErrorsTest {
+    private Game game;
+    private Task currentTask;
+    private ArrayList<Integer> answer;
+
     @Given("^Имеется экземпляр игры$")
     public void имеетсяЭкземплярИгры() {
+        game = new Game();
     }
 
     @Given("^В игре есть текущее задание$")
     public void вИгреЕстьТекущееЗадание() {
-
+        currentTask = game.getCurrentTask();
     }
 
-    @When("^Пользователь вводит кооректный ответ на задание$")
-    public void пользовательВводитКооректныйОтветНаЗадание() {
+    @When("^Пользователь вводит кооректный ответ на задание (\\d+)$")
+    public void пользовательВводитКооректныйОтветНаЗадание(int arg0) {
+        answer = new ArrayList<Integer>();
+        answer.add(arg0);
+    }
+
+    @Then("^Проверяется корректность ответа$")
+    public void проверяетсяКорректностьОтвета() {
+        boolean checkResult = game.checkAnswer(answer);
+        Assert.assertTrue(checkResult);
     }
 
     @Then("^Текущим заданием становится следующее задание в игре$")
     public void текущимЗаданиемСтановитсяСледующееЗаданиеВИгре() {
+        Assert.assertNotEquals(currentTask, game.getCurrentTask());
     }
+
+
 }
