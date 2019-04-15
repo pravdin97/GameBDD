6f37d0ed1c70bdc0a961bc071bacdd992291436e
pravdin97 <pravdin.ev97@gmail.com>
2019-04-15 16:06:34 +0700

    Сгенерирован файл с тестами

---
+++ src/main/java/FindErrorsTest.java
@@ -0,0 +1,22 @@
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class FindErrorsTest {
+    @Given("^Имеется экземпляр игры$")
+    public void имеетсяЭкземплярИгры() {
+    }
+
+    @Given("^В игре есть текущее задание$")
+    public void вИгреЕстьТекущееЗадание() {
+
+    }
+
+    @When("^Пользователь вводит кооректный ответ на задание$")
+    public void пользовательВводитКооректныйОтветНаЗадание() {
+    }
+
+    @Then("^Текущим заданием становится следующее задание в игре$")
+    public void текущимЗаданиемСтановитсяСледующееЗаданиеВИгре() {
+    }
+}
