5419ce5d2ce9d27d9419b26747b539505ef205cf
pravdin97 <pravdin.ev97@gmail.com>
2019-04-18 16:24:53 +0700

    Создан сценарий для чтения заданий и ответов к заданиям

---
+++ src/main/java/ReadTasksTest.java
@@ -0,0 +1,28 @@
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class ReadTasksTest {
+
+    @Given("^Путь к папке с заданиями на диске$")
+    public void путьКПапкеСЗаданиямиНаДиске() {
+    }
+
+    @Given("^Путь к папке с ответами на задания на диске$")
+    public void путьКПапкеСОтветамиНаЗаданияНаДиске() {
+    }
+
+    @When("^Создается экземпляр игры$")
+    public void создаетсяЭкземплярИгры() {
+    }
+
+    @Then("^Происходит чтение всех заданий$")
+    public void происходитЧтениеВсехЗаданий() {
+
+    }
+
+    @And("^Происходит чтение всех ответов к заданиям$")
+    public void происходитЧтениеВсехОтветовКЗаданиям() {
+    }
+}
