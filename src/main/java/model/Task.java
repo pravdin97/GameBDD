44d4156d9792e45eb15bb676ff5126269c2dc076
pravdin97 <pravdin.ev97@gmail.com>
2019-04-18 15:22:21 +0700

    Написан класс Task.

--- src/main/java/model/Task.java
+++ src/main/java/model/Task.java
@@ -1,4 +1,29 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Task {
+    private ArrayList<String> text;
+    private ArrayList<Integer> errorLineIndexes;
+
+    public Task() {
+        text = new ArrayList<String>();
+        errorLineIndexes = new ArrayList<Integer>();
+    }
+
+    public Task(ArrayList<String> text) {
+        this.text = text;
+        errorLineIndexes = new ArrayList<Integer>();
+    }
+    public ArrayList<String> getText() {
+        return text;
+    }
+
+    public void setErrorLineIndexes(ArrayList<Integer> errorLineIndexes) {
+        this.errorLineIndexes = errorLineIndexes;
+    }
+
+    public ArrayList<Integer> getErrorLineIndexes() {
+        return errorLineIndexes;
+    }
 }
